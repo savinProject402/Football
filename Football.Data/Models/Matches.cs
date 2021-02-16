7a1b2f5b612c80ef59923b40ca9c4038e127b816
Олег Савін <oleh.savin@nure.ua>
2021-02-17 01:21:00 +0200

    Добавьте файлы проекта.

---
+++ Football.Data/Models/Matches.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football.Data.Models
+{
+   public class Matches
+    {
+        public int Id { get; set; }
+        public string NameMatches { get; set; }
+
+        public virtual ICollection<Players> Players { get; set; }
+    }
+}
---
+++ Football.Data/Models/Players.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football.Data.Models
+{
+   public class Players
+    {
+        public int Id { get; set; }
+        public string NamePlayer { get; set; }
+        public int MatchesId { get; set; }
+
+        public virtual Matches Matches { get; set; }
+
+        public virtual ICollection<Cards> Cards { get; set; }
+    }
+}
