4617be9ce26a3608768371ee29ec438b77d7e2f2
Олег Савін <oleh.savin@nure.ua>
2021-02-17 05:10:56 +0200

    update DB v.1

---
+++ Football.Data/Migrations/202102170310055_create_addet_Cards_to_Players.cs
@@ -0,0 +1,16 @@
+﻿namespace Football.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class create_addet_Cards_to_Players : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
